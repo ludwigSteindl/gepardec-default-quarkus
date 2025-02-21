3a1dfac0baf5d61859ccdf79558bcc95d68042a5
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2025-02-21 10:28:14 +0100

    change back to junit4

--- src/test/java/org/acme/Junit4Test.java
+++ src/test/java/org/acme/Junit4Test.java
@@ -2,27 +2,27 @@ package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
-class Junit4Test {
+public class Junit4Test {
 
-    @BeforeEach
-    void setUp() {
+    @Before
+    public void setUp() {
         System.out.println("🚀 Testvorbereitung läuft...");
     }
 
-    @AfterEach
-    void tearDown() {
+    @After
+    public void tearDown() {
         System.out.println("✅ Test abgeschlossen.");
     }
 
     @Test
-    void helloEndpoint() {
+    public void testHelloEndpoint() {
         RestAssured.given()
                 .when().get("/hello")
                 .then()
